b7141d8a845886c8d430aa29f091cd32e1b58880
Anshikarathour64 <134735349+Anshikarathour64@users.noreply.github.com>
2024-01-09 21:32:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const wishListinputElement= document.querySelector(".input");
+const addwishlistbutton= document.querySelector(".button");
+let wishlistvalue="";
+const parentwishlistcontainer= document.querySelector(".wishlist-container");
+let wishlist=[];
+const form= document.querySelector(".form");
+wishListinputElement.addEventListener('keyup',(event) => {wishlistvalue=event.target.value;});
+form.addEventListener('submit',(event) => { event.preventDefault();
+wishlist=[...wishlist,wishlistvalue];wishListinputElement.value="";parentwishlistcontainer.innerHTML=""; showwishlist(wishlist);})
+
+const showwishlist = (wishlist) => {
+    for (let item of wishlist){
+        const containerelement= document.createElement('div');
+        const wishlisttextelement = document.createElement('span');
+        wishlisttextelement.innerText = item;
+        containerelement.appendChild(wishlisttextelement);
+        const deletebuttonelement= document.createElement('button');
+        deletebuttonelement.innerText='delete'
+        deletebuttonelement.setAttribute('name',item);
+        containerelement.appendChild(deletebuttonelement);
+        parentwishlistcontainer.appendChild(containerelement);
+    }
+} 
+parentwishlistcontainer.addEventListener('click',(event) => {
+    const itemtodelete=event.target.name;
+    wishlist=wishlist.filter(i => i!==itemtodelete);
+    parentwishlistcontainer.innerHTML='';
+    showwishlist(wishlist);
+})
